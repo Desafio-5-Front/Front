e38fc8c3f4d042548364cf1bb15ff3151ddf10e2
Pytwer <mariaraimundacarvalho1970@gmail.com>
2025-06-13 21:10:34 -0300

    feat: adicionando a seção de inicio da landing

--- conectasus/src/App.tsx
+++ conectasus/src/App.tsx
@@ -21,6 +21,26 @@ const App: React.FC = () => {
           </nav>
         </div>
       </header>
+      <section className="hero" style={{ height: '700px' }}>
+        <div className="hero-background" style={{ backgroundImage: `url(${homeImage})` }}></div>
+        <div className="container">
+          <div className="hero-content">
+            <div className="hero-left">
+              <h1 className="hero-title">ConectaSUS</h1>
+              <h2 className="hero-subtitle">Acesse informações relevantes de saúde</h2>
+              <p className="hero-description">
+                O ConectaSUS é um portal digital que facilita o acesso aos serviços do SUS. Consulte vacinação, exames,
+                atendimentos e encontre unidades de saúde de forma simples, rápida e integrada.
+              </p>
+              <div className="hero-buttons" style={{ marginLeft: "800px" }}>
+                <button className="btn" style={{ marginTop: "-300px" }}>Consultar unidades →</button>
+                <button className="btn" style={{ marginTop: "20px" }}>Dashboard →</button>
+                <button className="btn" style={{ marginTop: "20px" }}>Jogar agora →</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
